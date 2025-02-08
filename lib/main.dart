b0d14c45d7a7535a51c2eb0972848165bb07d1e4
Anouk Boukema <boukema.a@gmail.com>
2025-02-08 21:23:15 +0100

    textComponent

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'TextComponent.dart';
+import 'textComponent.dart';
 
 void main() {
   runApp(const MyApp());
